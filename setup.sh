af3e4ffa41e92876250e12d66ab2d86179cf2bc5
Ben Standerline <46759388+thelioncape@users.noreply.github.com>
2020-08-23 18:06:13 +0100

    Add comments to script and add copy for theme

--- setup.sh
+++ setup.sh
@@ -12,7 +12,12 @@ then
 	exit
 fi
 
+# Get script directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Install oh-my-zsh
 zsh -c "sh -c '(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)'"
+
+# Copy bullet-train theme to ZSH CUSTOM directory
+cp $DIR/bullet-train.zsh-theme $ZSH_CUSTOM/bullet-train.zsh-theme
 
