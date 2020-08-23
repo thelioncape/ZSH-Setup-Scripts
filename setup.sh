c42ceeb3527c40bcdd988864397759d58428365d
Ben Standerline <46759388+thelioncape@users.noreply.github.com>
2020-08-24 00:30:17 +0100

    Potential fix for #2

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Install oh-my-zsh
-sh -c '(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)'
+sh -c '$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)'
 
 # Copy bullet-train theme to ZSH CUSTOM directory
 cp $DIR/bullet-train.zsh-theme $HOME/.oh-my-zsh/custom/bullet-train.zsh-theme
