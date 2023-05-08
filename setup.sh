54d2cc4980b55f4f1641085ee7a01b5a9ffac884
thelioncape <46759388+thelioncape@users.noreply.github.com>
2023-05-08 17:32:12 +0100

    Add SSH Agent spawning to zshrc

--- setup.sh
+++ setup.sh
@@ -24,4 +24,29 @@ cp $DIR/bullet-train.zsh-theme $HOME/.oh-my-zsh/custom/bullet-train.zsh-theme
 # Modify .zshrc to use bullet-train theme
 sed -i 's~\(ZSH_THEME="\)[^"]*\(".*\)~\1bullet-train\2~' $HOME/.zshrc
 
+# Add SSH Agent Configuration to zshrc
+cat <<EOT >> ~/.zshrc
+SSH_ENV="\$HOME/.ssh/agent-environment"
+
+function start_agent {
+	echo "Initialising new SSH agent..."
+	/usr/bin/ssh-agent | sed 's/^echo/#echo/' > "\${SSH_ENV}"
+	echo succeeded
+	chmod 600 "\${SSH_ENV}"
+	. "\${SSH_ENV}" > /dev/null
+	/usr/bin/ssh-add;
+}
+
+# Source SSH settings, if applicable
+
+if [ -f "\${SSH_ENV}" ]; then
+	. "\${SSH_ENV}" > /dev/null
+	ps -ef | grep \${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+		start_agent;
+	}
+else
+	start_agent;
+fi
+EOT
+
 echo "Completed"
